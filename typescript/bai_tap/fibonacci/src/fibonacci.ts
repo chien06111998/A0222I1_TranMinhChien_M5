60f70c4e09e834f3361ebab01b54a5bc7b9d4697
chien06111998 <chien1998qb@gmail.com>
2022-11-30 21:27:22 +0700

    up

---
+++ typescript/bai_tap/fibonacci/src/fibonacci.ts
@@ -0,0 +1,16 @@
+function isFibo(number: number): number {
+    if (number == 1) return 0;
+    if (number == 2) return 1;
+    return isFibo(number - 1) + isFibo(n - 2);
+}
+
+const n = 100;
+var series : Array<number> = new Array<number>(n);
+series.fill(0);
+var sum = 0;
+for (let  i = 1; i < n; i++) {
+    // series[i-1] = isFibo(i);
+    sum += i;
+}
+console.log(series);
+console.log("Tổng các số fibonacci trong " + n + " phần tử là: " + sum);
