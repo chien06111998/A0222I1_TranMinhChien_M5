0d5029ced66651887216cc5c44a4ee1a3c86d5b3
chien06111998 <chien1998qb@gmail.com>
2022-12-05 16:32:05 +0700

    up

--- typescript/bai_tap/fibonacci/src/fibonacci.ts
+++ typescript/bai_tap/fibonacci/src/fibonacci.ts
@@ -1,7 +1,7 @@
 function isFibo(number: number): number {
     if (number == 1) return 0;
     if (number == 2) return 1;
-    return isFibo(number - 1) + isFibo(n - 2);
+    return isFibo(number - 1) + isFibo(number - 2);
 }
 
 const n = 100;
@@ -9,7 +9,7 @@ var series : Array<number> = new Array<number>(n);
 series.fill(0);
 var sum = 0;
 for (let  i = 1; i < n; i++) {
-    // series[i-1] = isFibo(i);
+    series[i-1] = isFibo(i);
     sum += i;
 }
 console.log(series);
